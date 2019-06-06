ad84bf563d8d9dd503d85b9d8780a0b5128beddd
Jose Daniel <josedanq100@gmail.com>
2019-06-06 11:19:24 -0400

    Main banner

---
+++ src/components/MainBanner.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import headerImg from '../assets/Screen_header.svg'
+import roundsBack from '../assets/rounds_back.svg'
+import roundsBack2 from '../assets/rounds_back_2.svg'
+import androidIcon from '../assets/android_icon_white.svg'
+import appleIcon from '../assets/apple_icon_black.svg'
+
+const MainBanner = () => {
+  return (
+    <section className='bg-purple-gradient main-banner d-flex'>
+      <h1 className='header-app-name'>Sleep magic</h1>
+      <img src={roundsBack} alt='' className='rounds-back' />
+      <img src={roundsBack2} alt='' className='rounds-back-2' />
+      <div className='left header-content'>
+        <h2 className='header-main-title'>Relax,<br /> sleep better and de-stress</h2>
+        <p className='header-main-paragraph'>Travel to beautiful places in your mind with beautiful sounds made to create harmony, peace and complete relaxation.</p>
+
+        <div className='download-buttons d-flex'>
+          <a href='javascript:;' className='header-btn d-flex link-no-style bg-pink'>
+            <img src={androidIcon} alt='android icon white' />
+            <p>available on <span class='store'>Google Store</span></p>
+          </a>
+          <a href='javascript:;' className='header-btn d-flex link-no-style bg-white'>
+            <img src={appleIcon} alt='apple icon dark' />
+            <p>available on <span class='store'>Apple Store</span></p>
+          </a>
+        </div>
+      </div>
+      <div className='right header-img'>
+        <img src={headerImg} alt='sleep magic header' />
+      </div>
+    </section>
+  )
+}
+
+export default MainBanner
---
+++ src/components/TermsAndConditions.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+
+const TermsAndConditions = () => {
+  return (
+    <section>
+      <h1>Terms & Conditions</h1>
+
+      <h2>1. GENERAL INFORMATION</h2>
+
+      <h3>1.1 Magictime Apps PRODUCTS</h3>
+      <p>
+				These Terms & Conditions (these “Terms”) contain the terms and conditions on which we supply content, products or services listed on www.magictimeapps.com (the “Website”), through our applications (the “Apps”) or via other delivery methods to you (the Website and such content, products, services and the Apps are collectively referred to herein as the “Product” or “Products”, which may be updated from time-to-time at the sole discretion of Magictime Apps). Please read these terms and conditions, carefully before ordering any Products from the Website or third party App stores (e.g. the Apple App Store, the Android Play Store,). The terms “Magictime Apps,” “us” or “we” refers to Magictime Apps,. and its wholly owned Irish based subsidiary, Magictime Apps Limited. The term “Device” refers to the device which is used to access the Products including but not limited to computers, smart-phones and tablets. The term “you” refers to the user of the Products. When you order (“Order”) any Products, or otherwise use or access the Products, you agree to be bound by these Terms and all applicable laws, rules and regulations. You may also be asked to click “I accept” at the appropriate place prior to your purchase of access to the Products. At such time, if you do not click “I accept”, you may not be able to complete such purchase or gain such access. By using the Products, you indicate that you accept these Terms and that you agree to abide by them. If you do not agree to these Terms, please refrain from using the Products.
+      </p>
+      <p>
+				Our contact email address is info@magictimeapps.com. All correspondence to Magictime Apps including any queries you may have regarding your use of the Products or these Terms should be sent to this contact email address.
+      </p>
+
+      <h3>1.2 ARBITRATION NOTICE AND CLASS ACTION WAIVER</h3>
+      <p>
+				PLEASE NOTE THAT THESE TERMS CONTAIN AN ARBITRATION CLAUSE. EXCEPT FOR CERTAIN TYPES OF DISPUTES MENTIONED IN THE ARBITRATION CLAUSE, YOU AND Magictime Apps AGREE THAT DISPUTES RELATING TO THESE TERMS OR YOUR USE OF THE PRODUCTS WILL BE RESOLVED BY MANDATORY BINDING ARBITRATION, AND YOU AND Magictime Apps WAIVE ANY RIGHT TO PARTICIPATE IN A CLASS-ACTION LAWSUIT OR CLASS-WIDE ARBITRATION.
+      </p>
+
+      <h3>1.3 BASIS OF LICENSE</h3>
+      <p>
+				(a) These Terms and the Order set out the whole agreement between you and us for the supply of the Products. In order to participate in certain Products, you may be required to agree to additional terms and conditions; those additional terms are hereby incorporated into these Terms. Where such terms are inconsistent with these Terms, the additional terms shall control.
+      </p>
+      <p>
+				(b) Please check that the details in these Terms and on the Order are complete and accurate before you use or commit yourself to purchase the Products. If you think that there is a mistake, please make sure that you ask us to confirm any changes in writing, as we only accept responsibility for statements and representations made in writing by an officer of Magictime Apps.
+      </p>
+      <p>
+				(c) AS PART OF YOUR USE OF THE PRODUCTS, YOU AFFIRMATIVELY CONSENT TO THE PROCESSING AND STORAGE OF YOUR PERSONAL INFORMATION IN THE UNITED STATES AND IRELAND, INCLUDING THE PROCESSING AND STORING OF YOUR PERSONAL INFORMATION IN THE UNITED STATES AND IRELAND FOR THE PURPOSES OF PROCESSING PAYMENTS AND TRACKING INDIVIDUAL USE OF THE PRODUCTS. BY USING THE PRODUCTS, YOU ACKNOWLEDGE THAT YOU UNDERSTAND AND AGREE THAT THE UNITED STATES AND IRELAND MAY NOT HAVE THE SAME LEVEL OF PROTECTIONS FOR YOUR PERSONAL INFORMATION THAT EXIST IN YOUR COUNTRY OF RESIDENCE, AND YOU NONETHELESS CONSENT TO THE PROCESSING AND STORAGE OF YOUR PERSONAL INFORMATION IN THE UNITED STATES AND THE UNITED KINGDOM. WE WILL TAKE MEASURES AS REQUIRED TO COMPLY WITH APPLICABLE LAW REGARDING THE TRANSFER, STORAGE AND USE OF CERTAIN PERSONAL INFORMATION.
+      </p>
+
+      <h3>1.4 CHANGES TO TERMS</h3>
+      <p>
+				Magictime Apps reserves the right to change or update these Terms, or any other of our policies or practices, at any time, and will notify users by posting such changed or updated Terms on this page. Any changes or updates will be effective immediately upon posting to www.magictimeapps.com. Your continued use of the Products constitutes your agreement to abide by the Terms as changed. Under certain circumstances we may also elect to notify you of changes or updates to our Terms by additional means, such as pop-up or push notifications within the Products or email.
+      </p>
+
+      <h2>2. MEMBERSHIPS AND SUBSCRIPTIONS</h2>
+
+      <h3>2.1 BECOMING A MEMBER</h3>
+      <p>
+				(a) You may sign up as a registered user of the Products free of charge (a “Member”). To become a Member you need to go to the relevant section of the Products, then submit your email address to us, and create a password to be used in conjunction with that email address. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your Device.
+      </p>
+      <p>
+				(b) In the course of your use of the Products, you may be asked to provide certain personalized information to us (such information is referred to hereinafter as “User Information”). This User Information may include information from your Facebook and similar social networking profiles. Our information collection and use policies with respect to the privacy of such User Information are set forth in the Magictime Apps Privacy Policy. You acknowledge and agree that you are solely responsible for the accuracy and content of User Information, and you agree to keep it up to date.
+      </p>
+      <p>
+				(c) By placing an Order through the Products, you warrant that:
+      </p>
+      <p>
+				(i) You are legally capable of entering into binding contracts; (ii) All registration information you submit is truthful and accurate; (iii) You will maintain the accuracy of such information; and (iv) Your use of the Products does not violate any applicable law or regulation.
+      </p>
+
+      <h3>2.2 ONCE A MEMBER</h3>
+      <p>
+				You are responsible for maintaining the confidentiality of your account, password and other User Information and for restricting access to your Device to further help protect such information. You are responsible for updating your User Information.
+      </p>
+
+      <h3>2.3 USE OF Magictime Apps</h3>
+      <p>
+				You must be 18 years of age, or the age of majority in your province, territory or country, to sign up as a registered user of the Products. Individuals under the age of 18, or the applicable age of majority, may utilize the Products only with the involvement and consent of a parent or legal guardian, under such person's account and otherwise subject to these Terms.
+      </p>
+
+      <h3>2.4 MEMBERSHIP</h3>
+      <p>
+				As a Magictime Apps Member you will receive access to certain sections, features and functions of the Products that are not available to non-members.
+      </p>
+
+      <p>
+				By agreeing to become a Member you opt-in to receiving occasional special offers, marketing, survey and Product based communication emails. You can easily unsubscribe from Magictime Apps commercial emails by following the opt-out instructions in the emails. Magictime Apps memberships and subscriptions are not transferable and therefore cannot be sold or exchanged or transferred in any way whatsoever.
+      </p>
+
+      <h3>2.5 SUBSCRIPTIONS</h3>
+      <p>(a) Magictime Apps account holders may access the Products in two ways:</p>
+      <p>(i) Free Trial: a free-of-charge program, which gives unlimited access to our free features and content.</p>
+      <p>
+				(ii) Paid Subscription: a subscription fee-based program, which gives access to all content including and beyond the Free Trial. You will only have access to the Subscription Program while your subscription is active and subsisting. You may have access to a free trial period of the Subscription Program in accordance with certain promotional offers. All subscription services provide access through the Products. You can become a subscriber to the Subscription Program by purchasing a subscription to the Products from the Website, within the Apps, where allowed by the App marketplace partners, or through a bundle with one or more of our bundle subscription partners. Please note that if you purchase a subscription through the Apple iTunes Store or our iPhone application, the sale is final, and we will not provide a refund. Your purchase will be subject to Apple’s applicable payment policy, which also may not provide for refunds. If you purchase a subscription through the Google Play store, the sale is final and we will not provide a refund. Your purchase will be subject to Google’s applicable payment policy, which also may not provide for refunds. If you purchase through one or more of our bundle subscription partners, the purchase may be further subject to the Terms and Conditions of such partners, and payment and management of the bundle subscription may be administered by them.
+      </p>
+      <p>
+				(b) Magictime Apps offers weekly, monthly, annual and forever subscription options. For the purposes of our monthly and yearly subscriptions, a month constitutes 30 calendar days and a year constitutes 365 calendar days. For the purposes of our forever subscription, forever constitutes 100 years or until the date Magictime Apps ceases to commercially offer the Products.
+      </p>
+      <p>
+				(c) Our “Monthly” subscription is paid in monthly installments. For each month that your monthly subscription is active, you acknowledge and agree that Magictime Apps is authorized to charge the same credit card as was used for the initial subscription fee or other payment method as set forth in section 2.5(h) (the “Payment Method”) in the amount of the current monthly subscription fee as of the time of renewal. The monthly renewal subscription fees will continue to be billed to the Payment Method you provided, automatically until cancelled. You must cancel your subscription before it renews each month in order to avoid billing of the next month’s subscription fee to the Payment Method you provided. Refunds cannot be claimed for any partial-month subscription period.
+      </p>
+      <p>
+				(d) Our “Yearly” subscription is paid for by an upfront one-off payment with automatic annual renewals. You acknowledge and agree that Magictime Apps is authorized to charge the Payment Method used for (i ) the initial annual subscription fee at the rate secured at the time of purchase, and (ii) the renewal subscription fee(s) at the non-discounted rate in effect at the time of any such renewal. You must cancel your subscription before it renews in order to avoid billing of the renewal subscription fee to the Payment Method you provided. Refunds cannot be claimed for any partial subscription period.
+      </p>
+      <p>
+				(e) Our “Forever” subscription is paid for by a one-off upfront payment.
+      </p>
+      <p>
+				(f) Subscription
+      </p>
+      <p>
+				(a) Magictime Apps offers certain special discount pricing options (the “Special Discount Pricing Options”). The Special Discount Pricing Options will permit users to access to the same content included in the Paid Subscription; such Special Discount Pricing Options shall only be available to qualified users (the “Qualified Users”). To be considered a Qualified User, your information will be provided directly Magictime Apps’s third-party verification system. Magictime Apps reserves the right to determine if you are a Qualifying User in our sole discretion.
+      </p>
+      <p>
+				(b) Magictime Apps Special Discount Pricing Options include the following:
+      </p>
+
+      <ol type='i'>
+        <li>The Student Discount Offer, the terms of which can be found in the Student Discount Offer Terms and Conditions.</li>
+        <li>The Magictime Apps Family plan, the terms of which can be found in the Magictime Apps Family Terms and Conditions.</li>
+      </ol>
+
+      <p>
+				(g) You agree to promptly notify Magictime Apps of any changes to the Payment Method you provided while any subscriptions remain outstanding. You are responsible for all applicable fees and charges incurred, including applicable taxes, and all subscriptions purchased by you.
+      </p>
+
+      <p>
+				(h) In the course of your use of the Products, Magictime Apps and its third party payment service provider may receive and implement updated credit card information from your credit card issuer in order to prevent your subscription from being interrupted by an outdated or invalid card. This disbursement of the updated credit card information is provided to Magictime Apps and Magictime Apps’s third party payment service provider at the sole election of your credit card issuer. Your credit card issuer may give you the right to opt-out of the update service. Should you desire to do so, please contact your credit card issuer.
+      </p>
+
+      <p>
+				(i) Our obligation to provide the Products only comes into being when we take receipt of your Order, and we confirm your purchase to you by email. We shall confirm your Order and send you an email to confirm your access to the subscription purchased. Please quote the Order number in all subsequent correspondence with us. Prices in US Dollars and Euros include local taxes. All prices in Pound Sterling include VAT unless otherwise stated. You agree not to hold us responsible for banking charges incurred due to payments on your account. If payment is not received by us from the Payment Method you provided, you agree to pay all amounts due upon demand by us. You agree that you are not permitted to resell any Products purchased through Magictime Apps for commercial purposes.
+      </p>
+
+      <h3>2.6 DEVICE REQUIREMENTS</h3>
+
+      <p>To enjoy Magictime Apps via your smartphone or other Device, your Device must satisfy certain system requirements. These requirements can be found on the Website and the Google & Apple App marketplaces.</p>
+
+      <h3>2.7 GIFTING</h3>
+
+      <p>“Gift Subscriptions” are pre-paid memberships to the Products. A person who purchases the gift is referred to in these terms as the “Giftor”. A person who receives and redeems a Gift Subscription to the Products is referred to in these terms as the “Recipient”. Gift subscriptions are paid for as a one-off upfront payment. Once bought, the Giftor will receive an Order confirmation and receipt. The Magictime Apps gift subscription will be sent to the Recipient on the Giftor’s specified date. Gifting codes can only be used once in the country for which they were purchased and cannot be redeemed for cash, resold or combined with any other offers, including free trial. Please note that gifting codes cannot be redeemed if the Recipient has already purchased a subscription through the Apple iTunes Store or our iPhone application, or the Google Play Store or our Android application. We will automatically bill the Payment Method you provided for any purchased Gift Subscriptions at the time of purchase, not delivery. There are no refunds or other credits for Gift Subscription that are not redeemed. Magictime Apps will notify the Recipient prior to the end of the Gift Subscription that the gift period is about to expire. Magictime Apps is not responsible if a Gift Subscription is lost, stolen or used without permission.
+      </p>
+
+      <h3>2.8 CORPORATE AND OTHER CONSUMER COMMUNITIES</h3>
+
+      <p>While Magictime Apps is a consumer products company, there is increasing interest by large consumer communities (corporations, universities, hospitals, etc.) (“Communities”) to introduce the Products to their employees and members. In some cases, these Communities may supplement these Terms with their own terms and conditions. In such event, these Community terms and conditions shall also apply to your use of the Products. In the event of any conflict with such additional terms and these Terms, these Terms shall prevail.
+      </p>
+
+      <h3>2.9 CHANGING FEES AND CHARGES</h3>
+      <p>We reserve the right to change our subscription plans or adjust pricing for our service or any components thereof in any manner and at any time as we may determine in our sole and absolute discretion. Except as otherwise expressly provided for in these Terms, any price changes or changes to your subscription plan will take effect following notice to you.
+      </p>
+
+      <h2>3. CANCELLATION OF SERVICES</h2>
+
+      <h3>3.1 CANCELLATION BY YOU</h3>
+
+    </section>
+  )
+}
+
+export default TermsAndConditions
