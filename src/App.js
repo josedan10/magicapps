f9867dd68bb0f9e9ff7dd24ad86b60b9df5e0471
Jose Daniel <josedanq100@gmail.com>
2019-06-07 10:19:02 -0400

    features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainBanner from './components/MainBanner'
+import Features from './components/Features'
 import './sass/app.scss'
 
 function App () {
   return (
     <div className='app'>
       <MainBanner />
+      <Features />
     </div>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import bg1 from '../assets/bg-1-features.svg'
+import bg2 from '../assets/bg-2-features.svg'
+import bg3 from '../assets/bg-3-features.svg'
+import iconSound from '../assets/icon_sounds.svg'
+import iconVideo from '../assets/icon_video.svg'
+import iconClock from '../assets/icon_clock.svg'
+
+const Features = () => (
+  <div className='features'>
+    <img src={bg3} alt='vector background' className='features__bg-3' />
+    <img src={bg2} alt='vector background' className='features__bg-2' />
+    <img src={bg1} alt='vector background' className='features__bg-1' />
+    <h2 className='features__title d-flex'>Awesome Features</h2>
+
+    <div className='features__container d-flex'>
+      <div className='features__container__card d-flex'>
+        <img src={iconSound} alt='' className='icon' />
+        <h3 className='name'>Beautiful Sounds</h3>
+        <p className='description'>Draft into deep paceful sleep with a combination of relaxing sounds.</p>
+      </div>
+      <div className='features__container__card d-flex'>
+        <img src={iconVideo} alt='' className='icon' />
+        <h3 className='name'>Inspiring Videos</h3>
+        <p className='description'>Be visually relaxed by wathing serene visions in nature, wonderful places from around the globe.</p>
+      </div>
+      <div className='features__container__card d-flex'>
+        <img src={iconClock} alt='' className='icon' />
+        <h3 className='name'>Alarm Clock & Timer</h3>
+        <p className='description'>Fall asleep with a timer and awake to beautiful music with an alarm clock.</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Features
